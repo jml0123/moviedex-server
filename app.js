9e867d69698fa87e0743878e4af49831134160cc
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-05-31 00:14:36 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+// USE THIS KEY FOR DEMO PURPOSES: 7c70d06c-a2dd-11ea-bb37-0242ac130002
+require('dotenv').config()
+const express = require('express');
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors")
+const MOVIE_DATA = require('./movies-data-small.json')
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(function validateToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get("Authorization")
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        res.status(401).json({error: "Unauthorized Request. Invalid API Key"})
+    }
+    next();
+})
+
+const handleGetMovies = (req, res) => {
+    const {genre, country, avg_vote, sort} = req.query;
+   
+    let response = MOVIE_DATA;
+
+    if (genre) {
+        response = response.filter(film => 
+            film.genre.toLowerCase().includes(genre.toLowerCase())
+        )
+    }
+    if (country) {
+        response = response.filter(film => 
+            film.country.toLowerCase().includes(country.toLowerCase()) 
+    )}
+    if (avg_vote) {
+        if (isNaN(avg_vote)){
+            return res
+            .status(400)
+            .send("Average vote must be a number value")
+        }
+        Number(avg_vote) === avg_vote;
+        if (avg_vote > 10 || avg_vote < 0) {
+            return res
+            .status(400)
+            .send("Average vote must be a number between 1 to 10")
+        }
+        response = response.filter(film =>
+           film.avg_vote >= avg_vote
+        )
+    }
+    if (!response.length) {
+        response = 'Cannot find any movies for this search'
+    }
+    
+    // Added a boolean sort query to sort from highest to lowest
+    if(sort) {
+        response.sort((a, b) => {
+        return a["avg_vote"] < b["avg_vote"] ? 1 : a["avg_vote"] > b["avg_vote"] ? -1 : 0
+    })};
+    return res.json(response)
+}
+
+app.get("/movie", handleGetMovies)
+
+module.exports = app;
+
---
+++ test/app.test.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+const supertest = require('supertest');
+const app = require('../app');
+const { expect } = require('chai');
+
+describe('GET/movies', () => {
+    it('should be 401 if invalid auth token is passed', () => {
+        return supertest(app)
+            .get("/movie")
+            .set("Authorization", `Bearer bad token`)
+            .expect(401, {error: "Unauthorized Request. Invalid API Key"})
+    })
+    it('should be 401 if no auth token is passed', () => {
+        return supertest(app)
+            .get("/movie")
+            .expect(401, {error: "Unauthorized Request. Invalid API Key"})
+    })
+
+    it('should return an array of movies', () => {
+        return supertest(app)
+            .get("/movie")
+            .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => {
+                expect(res.body).to.be.an('array');
+                const movie = res.body[0];
+                expect(movie).to.include.all.keys(
+                    'film_title', 'year', 'genre', 'duration', 'country', 'director',
+                    'actors', 'avg_vote', 'votes'
+                )
+            })
+    })
+    it('should be 400 if invalid avg_vote query', () => {
+        return supertest(app)
+            .get("/movie")
+            .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
+            .query({avg_vote: "bad query"})
+            .expect(400, "Average vote must be a number value")
+         
+    })
+    it('should be 400 if avg_vote query is out of range', () => {
+        return supertest(app)
+            .get("/movie")
+            .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
+            .query({avg_vote: 22})
+            .expect(400, 'Average vote must be a number between 1 to 10')
+    })
+    it('should be 400 if avg_vote query is out of range', () => {
+        return supertest(app)
+            .get("/movie")
+            .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
+            .query({avg_vote: -3})
+            .expect(400, 'Average vote must be a number between 1 to 10')
+    })
+    it('should indicate if no matches exist', () => {
+        return supertest(app)
+            .get("/movie")
+            .set("Authorization", `Bearer ${process.env.API_TOKEN}`)
+            .query({genre: "does not exist"})
+            .then(res => {
+                expect(res.body).to.be.equal("Cannot find any movies for this search")
+            })
+    })
+})
